b58ee22df2052420b18d635390d60dfe724f6199
viego777 <henriquebrunogomes123@gmail.com>
2025-02-21 00:38:00 -0400

    fix: initial page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>Layout</h1>
         {children}
       </body>
     </html>
