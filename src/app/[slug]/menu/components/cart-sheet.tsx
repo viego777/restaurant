4254ef94f4798974926bab137f7b6fdc34f583ec
viego777 <henriquebrunogomes123@gmail.com>
2025-02-23 03:58:59 -0400

    feat: add initial add to cart function to cart contexts

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { CartContext } from "../contexts/cart";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart } = useContext(CartContext);
+    const { isOpen, toggleCart, products } = useContext(CartContext);
     return ( 
         <Sheet open={isOpen} onOpenChange={toggleCart}>
             <SheetTrigger>Open</SheetTrigger>
@@ -15,6 +15,11 @@ const CartSheet = () => {
                         and remove your data from our servers.
                     </SheetDescription>
                     </SheetHeader>
+                    {products.map((product)=>(
+                        <h1 key={product.id}>
+                            {product.name}-{product.quantity}
+                        </h1>
+                    ))}
                 </SheetContent>
         </Sheet>
      );
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -3,7 +3,7 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Product {
+interface CartProduct extends Pick<Product, "id" | "name" | "price"| "imageUrl"> {
     quantity: number;
 }
 
@@ -11,12 +11,14 @@ export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
     toggleCart: () => void;
+    addProduct: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
     toggleCart: () => {},
+    addProduct: () => {},
 });
 
 export const CartProvider = ({ children }: {children: ReactNode}) => {
@@ -26,12 +28,16 @@ export const CartProvider = ({ children }: {children: ReactNode}) => {
     const toggleCart = () => {
         setIsOpen(prev => !prev);
     };
+    const addProduct = (product: CartProduct) => {
+        setProducts((prev) =>([...prev, product])); 
+    };
     return (
         <CartContext.Provider
         value={{
             isOpen,
             products,
             toggleCart,
+            addProduct,
         }}
         >
             {children}
