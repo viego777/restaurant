c4e6ef0bbe90d0bc24d3fe7c721f6f346b199b55
viego777 <henriquebrunogomes123@gmail.com>
2025-02-23 23:20:07 -0400

    feat: ensure cart total is shown

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -2,19 +2,33 @@ import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTr
 import { useContext } from "react";
 import { CartContext } from "../contexts/cart";
 import CartProductItem from "./cart-product-item";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/helpers/format-currency";
 
 const CartSheet = () => {
-    const { isOpen, toggleCart, products } = useContext(CartContext);
+    const { isOpen, toggleCart, products, total } = useContext(CartContext);
     return ( 
         <Sheet open={isOpen} onOpenChange={toggleCart}>
                 <SheetContent className="w-[80%]">
                     <SheetHeader>
                     <SheetTitle className="text-left">Sacola</SheetTitle>
                     </SheetHeader>
-                    <div className="py-5">
+                    <div className="h-full flex flex-col py-5">
+                    <div className="flex-auto">
                     {products.map((product)=>(
                         <CartProductItem key={product.id} product={product}/>
                     ))}
+                    </div>
+                    <Card className="mb-6">
+                        <CardContent className="p-5">
+                            <div className="flex justify-between">
+                                <p className="text-sm text-muted-foreground">Total</p>
+                                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Button className="w-full rounded-full">Finalizar pedido</Button>
                     </div>
                 </SheetContent>
         </Sheet>
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -10,6 +10,7 @@ export interface CartProduct extends Pick<Product, "id" | "name" | "price"| "ima
 export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
+    total: number;
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
     decreaseProductQuantity: (productId: string) => void;
@@ -19,6 +20,7 @@ export interface ICartContext {
 
 export const CartContext = createContext<ICartContext>({
     isOpen: false,
+    total: 0,
     products: [],
     toggleCart: () => {},
     addProduct: () => {},
@@ -31,6 +33,9 @@ export const CartProvider = ({ children }: {children: ReactNode}) => {
     const [products, setProducts] = useState<CartProduct[]>([]);
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
+    const total = products.reduce((acc, product) => {
+        return acc + product.price * product.quantity;
+    } , 0)
     const toggleCart = () => {
         setIsOpen(prev => !prev);
     };
@@ -92,6 +97,7 @@ export const CartProvider = ({ children }: {children: ReactNode}) => {
             decreaseProductQuantity,
             increaseProductQuantity,
             removeProduct,
+            total,
         }}
         >
             {children}
